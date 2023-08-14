39f0390a10c409ea332189f3e192b7b1b210fd70
Stanley Karanja <archymaseno@gmail.com>
2023-08-14 03:32:06 +0300

    More tests

---
+++ ElevatorSystem.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using ElevatorSystem.Services;
+global using ElevatorSystem.Models;
+global using ElevatorSystem.Data.Enums;
+global using ElevatorSystem.Abstracts.Interfaces;
